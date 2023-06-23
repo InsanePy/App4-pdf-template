2acebaf9cb9a2a17ab090e16375dae3043a0d855
StudioCrew-design <dev.studiocrew@gmail.com>
2023-06-23 17:59:48 +0530

    Added footer to all Pages

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from fpdf import FPDF
 import pandas
 pdf = FPDF(orientation="P", unit="mm", format="A4")
-
+pdf.set_auto_page_break(auto=False,margin=0)
 df = pandas.read_csv("topics.csv")
 
 for index, row in df.iterrows():
-
+    #Master Page
     pdf.add_page()
+
     pdf.set_font(family="Times", style="B", size=12)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 20, 200, 20)
 
+   # Footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=10)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
     for i in range(row["Pages"]-1):
         pdf.add_page()
-
+        # Footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=10)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 pdf.output("output.pdf")
 
 
