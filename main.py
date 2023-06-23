da36b5f9e2d57d6a4783e47890d9f54b9534e32c
StudioCrew-design <dev.studiocrew@gmail.com>
2023-06-23 17:16:59 +0530

    create multi page pdf with headers and underline from csv file using pandas lib

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fpdf import FPDF
-
+import pandas
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello There", align="L", ln=1)
+df = pandas.read_csv("topics.csv")
 
-pdf.add_page()
-pdf.set_font(family="Times", size=10)
-pdf.cell(w=0, h=12, txt="Hi There", align="L", ln=1)
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(10, 20, 200, 20)
 
 pdf.output("output.pdf")
 
