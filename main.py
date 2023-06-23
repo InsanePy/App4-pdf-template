81b6d8a88fdc14235b3e9f45b75a9943fdc2501f
StudioCrew-design <dev.studiocrew@gmail.com>
2023-06-23 18:27:19 +0530

    Added lines to all Pages

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ for index, row in df.iterrows():
     pdf.line(x1, y1, x2, y2)
 
     # Adding lines to pdf
-    #for i in range(20, 298, 10):
-        #pdf.line(x1, i, x2, i)
+    for i in range(20, 298, 10):
+        pdf.line(x1, i, x2, i)
 
    # Footer
     pdf.ln(265)
@@ -32,15 +32,16 @@ for index, row in df.iterrows():
     for i in range(row["Pages"]-1):
         pdf.add_page()
 
-        #Adding lines to pdf
-        #for i in range(20, 298, 10):
-            #pdf.line(x1, i, x2, i)
-
         # Footer
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=10)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        #Adding lines to pdf
+        for i in range(20, 298, 10):
+            pdf.line(x1, i, x2, i)
+
 
 pdf.output("output.pdf")
 #pdf.output("output1.pdf")
